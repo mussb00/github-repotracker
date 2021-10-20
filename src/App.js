1584c7cab998b875b26ae50a72626abb7740114d
mussb00 <berhanem2000@gmail.com>
2021-10-20 13:58:10 +0100

    styled the input and buttons, created a context provider for applicationState

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import { useState } from 'react';
 import HomePage from './Components/HomePage';
 import Repositories from './Components/Repositories';
-
+import { RepoContext } from './Context/Contexts';
 function App() {
   const [appState, setAppState] = useState('home')
   return (
     <div className="App">
       {/* import material-ui navbar here, black background color, white text, github logo, some redundant links */}
+      <RepoContext.Provider value={{appState, setAppState}}>
       {appState === 'home' && <HomePage/>}
       {appState === 'repositories' && <Repositories/>}
+      </RepoContext.Provider>
     </div>
   );
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
-import {Button} from '@mui/material'
+import {Button, TextField, InputAdornment} from '@mui/material'
+import {AccountCircle} from '@mui/icons-material'
 
 const HomePage = () => {
     const [userName, setUserName] = useState('')
+    
     const handleInputChange = e =>{
         setUserName(e.target.value)
     }
+
+    const handleSubmit = () => {
+        // if fetch returns error display the error (e.g. 'no user')
+        // otherwise change appState and 
+        setUserName('')
+    }
     return (
         <div>
-            <input type="text" placeholder="GitHub Username ..." onChange={handleInputChange}></input>
-            <Button variant="contained">Search</Button>
+            <form>
+            <TextField 
+            variant="filled" 
+            label="GitHub Username" 
+            onChange={handleInputChange}
+            InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}/>
+            <Button variant="contained" color="success" onClick={handleSubmit}>Search</Button>
+            </form>
         </div>
     )
 }
