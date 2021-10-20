39ae8e0f92912d5eb7359aad01c7e956cb240324
mussb00 <berhanem2000@gmail.com>
2021-10-20 12:58:32 +0100

    downloaded material ui, started making form

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
+import {Button} from '@mui/material'
 
 const HomePage = () => {
+    const [userName, setUserName] = useState('')
+    const handleInputChange = e =>{
+        setUserName(e.target.value)
+    }
     return (
         <div>
-            
+            <input type="text" placeholder="GitHub Username ..." onChange={handleInputChange}></input>
+            <Button variant="contained">Search</Button>
         </div>
     )
 }
